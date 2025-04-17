c144c5013fff4546cbd2c1148c9a07a1a0578003
Niyi Ogungbemi <niyiment@gmail.com>
2025-04-17 19:19:55 +0100

    project setup

---
+++ src/main/java/com/niyiment/gym/exception/AlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.niyiment.gym.exception;
+
+public class AlreadyExistsException extends RuntimeException {
+    public AlreadyExistsException(String message) {
+        super(message);
+    }
+}
